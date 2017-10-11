b400f0a2dffcbed5b6e10aa1039a5625e11ef61e
ivanberry <tabvim.dev@gmail.com>
2017-10-12 06:11:57 +0800

    (.all)Add unique id, throttle for expensive function

--- actions/index.js
+++ actions/index.js
@@ -1,10 +1,11 @@
+import {v4} from 'node-uuid';
+
 //actions are plain object with infomations will send to the store
 //action creator is the function return a actions
 //if you want to send the action to the store, you need dispatch 
-let nextTodoId = 0;
 export const addTodo = text => ({
   type: "ADD_TODO",
-  id: nextTodoId++,
+  id: v4(),
   text
 });
 
--- app.js
+++ app.js
@@ -7,17 +7,18 @@ import { Provider, connect } from "react-redux";
 import rooterReducer from "./reducers";
 import TodoApp from "./components/TodoApp";
 import { loadState, saveState } from "./localStore";
+import throttle from 'lodash/throttle';
 
 const persistedState = loadState();
 
 const store = createStore(rooterReducer, persistedState);
 console.log(store.getState());
 
-store.subscribe(() => {
+store.subscribe(throttle(() => {
   saveState({
     todos: store.getState().todos //just save the data without any UI state
   });
-});
+}, 1000));
 
 ReactDOM.render(
   <Provider store={store}>
